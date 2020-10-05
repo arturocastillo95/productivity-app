f7d9b2301fbffd48f01023476f254cd4707ca8d1
Arturo Castillo <arturguay@gmail.com>
2020-10-05 05:02:28 -0500

    Correct file location

---
+++ Procfile
@@ -0,0 +1,2 @@
+
+web: gunicorn productivityapp.wsgi --log-file -
---
+++ build/precache-manifest.aaf32390ae0e53962ebfb126eac74d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e31e82cabd8995bc61c5530fcb5af95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8df9fdcfd62b8f11eb92",
+    "url": "/static/css/main.d213303a.chunk.css"
+  },
+  {
+    "revision": "deeb7ef68c3c7aee68e7",
+    "url": "/static/js/2.6837aaba.chunk.js"
+  },
+  {
+    "revision": "bbacaf6541923629e3effd88e939419d",
+    "url": "/static/js/2.6837aaba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df9fdcfd62b8f11eb92",
+    "url": "/static/js/main.9ce79101.chunk.js"
+  },
+  {
+    "revision": "b2469bcae364f5aad51d",
+    "url": "/static/js/runtime-main.b6506af1.js"
+  }
+]);
